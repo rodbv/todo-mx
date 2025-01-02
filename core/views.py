1af4255ef2f9940f54e46838ee9b3b36ea3421c3
Rodrigo Vieira <rodrigo.vieira@gmail.com>
2025-01-02 14:58:35 -0300

    part 03 before HTMX

--- core/views.py
+++ core/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+# core/views.py
 
-# Create your views here.
+from django.shortcuts import redirect, render
+from .models import UserProfile, Todo
+from django.contrib.auth.decorators import login_required
+
+
+def index(request):
+    return redirect("tasks/")
+
+
+def get_user_todos(user: UserProfile) -> list[Todo]:
+    return user.todos.all().order_by("created_at")
+
+
+@login_required
+def tasks(request):
+    context = {
+        "todos": get_user_todos(request.user),
+        "fullname": request.user.get_full_name() or request.user.username,
+    }
+
+    return render(request, "tasks.html", context)
