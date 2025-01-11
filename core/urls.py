79a00cfe38548f3d025274ab373761d97affadd1
Rodrigo Vieira <rodrigo.vieira@gmail.com>
2025-01-11 19:07:26 -0300

    part 09 final

--- core/urls.py
+++ core/urls.py
@@ -8,6 +8,6 @@ urlpatterns = [
     path("tasks/", views.tasks, name="tasks"),
     path("tasks/<int:task_id>/toggle/", views.toggle_todo, name="toggle_todo"),
     path("tasks/<int:task_id>/", views.task_details, name="task_details"),
-    # NEW
     path("tasks/<int:task_id>/edit/", views.edit_task, name="edit_task"),
+    path("tasks/search/", views.search, name="search"),
 ]
